7ef7636f4b6c8e4e9a65f3f06c7ec74bd61a7b33
hussein <hussein66253@icloud.com>
2020-09-08 01:48:03 +0300

    note.test.js done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 const Input = require('./lib/input.js');
-const note = require('./lib/notes.js');
-
+const Note = require('./lib/notes.js');
+const note = new Note()
 const options = new Input();
 
 note.execute(options);

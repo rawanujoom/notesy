6c39e15f6d847059d8034f838af725c2d9be8b0c
hussein <hussein66253@icloud.com>
2020-09-08 22:49:10 +0300

    some editing

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,14 +1,14 @@
-'use strict'
+'use strict';
 const Note = require('../lib/notes.js');
 jest.spyOn(global.console, 'log');
 
 describe('the Note module', () => {
 
-    it('execute() check for valid method console it', () =>{
+  it('execute() check for valid method console it', () =>{
 
-        const note = new Note();
-        note.execute({add : 'note'});
-        expect(console.log).toHaveBeenCalled()
-    })
+    const note = new Note();
+    note.execute({add : 'note'});
+    expect(console.log).toHaveBeenCalled();
+  });
 
-})
+});

1b2b5bdb388efe8f60a20490641624441409b35b
hussein <hussein66253@icloud.com>
2020-09-09 15:37:03 +0300

    some edite

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const mongoose = require ('mongoose');
 const MONGOOSE_URL = 'mongodb://localhost:27017/nots';
 mongoose.connect(MONGOOSE_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    useUnifiedTopology: true
 });
 const Input = require('./lib/input.js');
 const Note = require('./lib/notes.js');

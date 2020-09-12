55f1fb85a2f92ba0d3a437ac635f0c1c18f25807
hussein <hussein66253@icloud.com>
2020-09-12 13:39:05 +0300

    class03 done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 const mongoose = require ('mongoose');
-const MONGOOSE_URL = 'mongodb://localhost:27017/nots';
+const MONGOOSE_URL = 'mongodb://localhost:27017/notes';
 mongoose.connect(MONGOOSE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -12,3 +12,7 @@ const note = new Note();
 const options = new Input();
 
 note.execute(options);
+// note.deleteNote('5f5b5ffd7f8e9238902f1038');
+console.log('*/*/**/*/****/**/****/');
+note.list({})
+console.log('*/*/**/*/****/**/****/');

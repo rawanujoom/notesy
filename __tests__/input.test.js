a679fcf171f72d7844359dad432acd83263c3651
hussein <hussein66253@icloud.com>
2020-09-12 13:56:10 +0300

    fix with lint

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -1,28 +1,28 @@
 jest.mock('minimist');
-const minimist = require ('minimist')
+const minimist = require ('minimist');
 minimist.mockImplementation(()=>{
-    return{
-        any:'any',
-        add :'note'
-    }
-})
+  return{
+    any:'any',
+    add :'note'
+  };
+});
 const Input = require('../lib/input.js');
 describe('the Input module', ()=>{
-it('valid() will check if the it is validated or not',()=>{
+  it('valid() will check if the it is validated or not',()=>{
     let test = new Input();
     expect(test.valid()).toBeTruthy();
-})
-it('checknote() will check if the it is validated',()=>{
+  });
+  it('checknote() will check if the it is validated',()=>{
     let test = new Input();
     expect(test.checknote()).toBeTruthy();
-})
-it('checkMethod() will check if the it is validated',()=>{
+  });
+  it('checkMethod() will check if the it is validated',()=>{
     let test = new Input();
     expect(test.checkMethod('--add')).toEqual('--add');
-})
-it('checkMethod() will check if the it is validated',()=>{
+  });
+  it('checkMethod() will check if the it is validated',()=>{
     let test = new Input();
     expect(test.checkMethod('-a')).toEqual('-a');
-})
+  });
 
-})
+});
